89c17d26140134f5d4d430d0bc5952c892bfe557
Maria Eduarda <119744320+madddss@users.noreply.github.com>
2023-06-07 12:15:17 -0300

    Add files via upload

---
+++ multiplos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aluno_aprovado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine(" São Múltiplos ");
+            }
+            else
+            {
+                Console.WriteLine(" Não são Múltiplos ");
+            }
+
+        }
+    }
+}
