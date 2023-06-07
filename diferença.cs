ed546a9e995ec0aa5b734c27f3229c35ffaaa4c1
Maria Eduarda <119744320+madddss@users.noreply.github.com>
2023-06-07 12:04:18 -0300

    Add files via upload

---
+++ diferença.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diferença
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f, i, o, p;
+            Console.WriteLine("Digite o primeiro valor : ");
+            f = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor:");
+            i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro valor: ");
+            o = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o quarto valor ");
+            p = int.Parse(Console.ReadLine());
+
+            int dif = (f * i - o * p);
+            Console.WriteLine("A diferença entre o produto F e I é de: " + dif);
+
+
+        }
+
+    }
+}
