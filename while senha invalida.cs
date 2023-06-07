1464658bce053ee2592ce6e375ddc0dfc541408e
Maria Eduarda <119744320+madddss@users.noreply.github.com>
2023-06-07 12:19:56 -0300

    Add files via upload

---
+++ while senha invalida.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1114
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+
+                Console.WriteLine("Senha inválida");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha válida");
+
+        }
+    }   } 
+
